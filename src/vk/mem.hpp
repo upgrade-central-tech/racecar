7b801d8174ca261d7e746d009c308b2f2758fcda
Charles Wang <zwcharl@gmail.com>
2025-11-13 18:06:30 -0500

    feat: convert `vk::mem::create_buffer`

--- src/vk/mem.hpp
+++ src/vk/mem.hpp
@@ -3,8 +3,6 @@
 #include "common.hpp"
 #include "vma.hpp"
 
-#include <optional>
-
 namespace racecar::vk::mem {
 
 struct AllocatedBuffer {
@@ -21,20 +19,13 @@ struct AllocatedImage {
     VkFormat image_format = VK_FORMAT_UNDEFINED;
 };
 
-struct DebugImageData {
-    std::optional<vk::mem::AllocatedImage> white_image;
-    std::optional<vk::mem::AllocatedImage> checkerboard_image;
-
-    VkSampler default_sampler_linear;
-};
-
 struct UniformBuffer {
     AllocatedBuffer buffer;
     size_t data_size = 0;
     void* mapped_data = nullptr;
 };
 
-std::optional<mem::AllocatedBuffer> create_buffer( Common& vulkan, size_t alloc_size,
-    VkBufferUsageFlags usage_flags, VmaMemoryUsage memory_usage );
+AllocatedBuffer create_buffer( Common& vulkan, size_t alloc_size, VkBufferUsageFlags usage_flags,
+    VmaMemoryUsage memory_usage );
 
 } // namespace racecar::vk::mem
