563a4f45c57b5cd7833801e7ffe1f71c76c05b90
Charles Wang <zwcharl@gmail.com>
2025-11-17 02:20:00 -0500

    feat: set up multiple slang files, hard code earth atmosphere parameters, add `get_sky_radiance` stump

--- shaders/atmosphere/compile_atmosphere.ps1
+++ shaders/atmosphere/compile_atmosphere.ps1
@@ -1,2 +1,2 @@
 # Assumes compiler is on your PATH (which it should be)
-slangc.exe "$PSScriptRoot\atmosphere.slang" -target spirv -profile spirv_1_4 -o "$PSScriptRoot\atmosphere.spv"
+slangc.exe "$PSScriptRoot\main.slang" -target spirv -profile spirv_1_4 -o "$PSScriptRoot\atmosphere.spv"
