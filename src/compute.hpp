54d29dea9372f523ea5269f304c1e03c2d9fb86b
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2025-11-21 15:32:58 -0500

    commpute baseline

--- src/compute.hpp
+++ src/compute.hpp
@@ -1,25 +1,21 @@
 #pragma once
 
+#include "engine/descriptor_set.hpp"
 #include "engine/pipeline.hpp"
 
 #include <glm/glm.hpp>
 
 namespace racecar::engine {
 
-struct ComputeCall {
-    VkShaderModule compute_shader = VK_NULL_HANDLE;
-    Pipeline pipeline;
-};
-
 struct ComputeTask {
-    std::vector<ComputeCall> compute_calls;
+    Pipeline pipeline;
+    std::vector<DescriptorSet*> descriptor_sets;
+
+    glm::ivec3 group_size;
 };
 
-std::optional<ComputeTask> create_compute_task();
+ComputeTask create_compute_task();
 void execute_compute_task();
 void add_compute_call( const ComputeTask& compute_call );
-
-std::optional<ComputeCall> create_compute_call( const VkShaderModule& compute_shader );
-void dispatch_compute_call( const ComputeTask& task );
 
 } // namespace racecar::engine
