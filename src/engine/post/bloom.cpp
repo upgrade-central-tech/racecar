e47385cd8bb269e4536ad696eb4438219b746f93
Charles Wang <zwcharl@gmail.com>
2025-11-24 07:28:49 -0500

    feat: full two-pass blur

--- src/engine/post/bloom.cpp
+++ src/engine/post/bloom.cpp
@@ -10,6 +10,7 @@ namespace {
 constexpr std::string_view BRIGHTNESS_THRESHOLD_PATH
     = "../shaders/post/bloom/brightness_threshold.spv";
 constexpr std::string_view HORZ_BLUR_PATH = "../shaders/post/bloom/horz_blur.spv";
+constexpr std::string_view VERT_BLUR_PATH = "../shaders/post/bloom/vert_blur.spv";
 
 }
 
@@ -102,25 +103,77 @@ BloomPass add_bloom( vk::Common& vulkan, const State& engine, TaskList& task_lis
     }
 
     {
-        engine::DescriptorSet blur_desc_set = engine::generate_descriptor_set( vulkan, engine,
+        engine::DescriptorSet horz_blur_desc_set = engine::generate_descriptor_set( vulkan, engine,
             { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE },
             VK_SHADER_STAGE_COMPUTE_BIT );
-        engine::update_descriptor_set_rwimage( vulkan, engine, blur_desc_set,
+        engine::update_descriptor_set_rwimage( vulkan, engine, horz_blur_desc_set,
             pass.brightness_threshold, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, 0 );
         engine::update_descriptor_set_rwimage(
-            vulkan, engine, blur_desc_set, output, VK_IMAGE_LAYOUT_GENERAL, 1 );
+            vulkan, engine, horz_blur_desc_set, pass.horz_blur, VK_IMAGE_LAYOUT_GENERAL, 1 );
 
-        engine::Pipeline blur_pipeline
-            = engine::create_compute_pipeline( vulkan, { blur_desc_set.layouts[0] },
+        engine::Pipeline horz_blur_pipeline
+            = engine::create_compute_pipeline( vulkan, { horz_blur_desc_set.layouts[0] },
                 vk::create::shader_module( vulkan, HORZ_BLUR_PATH ), "cs_main" );
 
         pass.horz_blur_desc_set
-            = std::make_unique<engine::DescriptorSet>( std::move( blur_desc_set ) );
+            = std::make_unique<engine::DescriptorSet>( std::move( horz_blur_desc_set ) );
 
         engine::add_cs_task( task_list,
             {
-                .pipeline = blur_pipeline,
+                .pipeline = horz_blur_pipeline,
                 .descriptor_sets = { pass.horz_blur_desc_set.get() },
+                .group_size = glm::ivec3( dims, 1 ),
+            } );
+    }
+
+    // From now on, we ping pong back and forth between our two already existing images. Now,
+    // we will read from the horizontal blur texture, and write to the brightness threshold texture.
+    {
+        engine::add_pipeline_barrier( task_list,
+            { .image_barriers = {
+                  {
+                      .src_stage = VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT,
+                      .src_access = VK_ACCESS_2_SHADER_WRITE_BIT,
+                      .src_layout = VK_IMAGE_LAYOUT_GENERAL,
+                      .dst_stage = VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT,
+                      .dst_access = VK_ACCESS_2_SHADER_READ_BIT,
+                      .dst_layout = VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL,
+                      .image = pass.horz_blur,
+                      .range = engine::VK_IMAGE_SUBRESOURCE_RANGE_DEFAULT_COLOR,
+                  },
+                  {
+                      .src_stage = VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT,
+                      .src_access = VK_ACCESS_2_SHADER_READ_BIT,
+                      .src_layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+                      .dst_stage = VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT,
+                      .dst_access = VK_ACCESS_2_SHADER_WRITE_BIT,
+                      .dst_layout = VK_IMAGE_LAYOUT_GENERAL,
+                      .image = pass.brightness_threshold,
+                      .range = engine::VK_IMAGE_SUBRESOURCE_RANGE_DEFAULT_COLOR,
+                  },
+              } } );
+    }
+
+    {
+        engine::DescriptorSet vert_blur_desc_set = engine::generate_descriptor_set( vulkan, engine,
+            { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE },
+            VK_SHADER_STAGE_COMPUTE_BIT );
+        engine::update_descriptor_set_rwimage( vulkan, engine, vert_blur_desc_set, pass.horz_blur,
+            VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, 0 );
+        engine::update_descriptor_set_rwimage(
+            vulkan, engine, vert_blur_desc_set, output, VK_IMAGE_LAYOUT_GENERAL, 1 );
+
+        engine::Pipeline vert_blur_pipeline
+            = engine::create_compute_pipeline( vulkan, { vert_blur_desc_set.layouts[0] },
+                vk::create::shader_module( vulkan, VERT_BLUR_PATH ), "cs_main" );
+
+        pass.vert_blur_desc_set
+            = std::make_unique<engine::DescriptorSet>( std::move( vert_blur_desc_set ) );
+
+        engine::add_cs_task( task_list,
+            {
+                .pipeline = vert_blur_pipeline,
+                .descriptor_sets = { pass.vert_blur_desc_set.get() },
                 .group_size = glm::ivec3( dims, 1 ),
             } );
     }
--- src/engine/post/bloom.hpp
+++ src/engine/post/bloom.hpp
@@ -12,10 +12,11 @@ namespace racecar::engine::post {
 
 struct BloomPass {
     RWImage brightness_threshold;
-    std::unique_ptr<engine::DescriptorSet> brightness_threshold_desc_set;
-
     RWImage horz_blur;
+
+    std::unique_ptr<engine::DescriptorSet> brightness_threshold_desc_set;
     std::unique_ptr<engine::DescriptorSet> horz_blur_desc_set;
+    std::unique_ptr<engine::DescriptorSet> vert_blur_desc_set;
 };
 
 BloomPass add_bloom( vk::Common& vulkan, const State& engine, TaskList& task_list,
