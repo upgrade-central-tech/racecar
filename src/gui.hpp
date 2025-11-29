8ebf6ac92572efa217077a5f6b78266e22db6b1b
Anthony Ge <geant@seas.upenn.edu>
2025-11-29 03:36:40 -0500

    improved ao very slightly

--- src/gui.hpp
+++ src/gui.hpp
@@ -47,9 +47,9 @@ struct Gui {
     } atms = {};
 
     struct AoData {
-        float thickness = 0.824f;
-        float radius = 0.100f;
-        float offset = 0.106f;
+        float thickness = 1.0f;
+        float radius = 0.20f;
+        float offset = 0.20f;
         bool debug_enable = false;
     } ao = {};
 };
