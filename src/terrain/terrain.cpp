a7b7606ddc84d08a3fcb9bd536b24c0c5faca40d
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2025-12-01 17:27:16 -0500

    big car

--- src/terrain/terrain.cpp
+++ src/terrain/terrain.cpp
@@ -26,7 +26,7 @@ void initialize_terrain( vk::Common& vulkan, engine::State& engine, Terrain& ter
     [[maybe_unused]] int32_t size = 1;
 
     float scale = 10.0f;
-    float offset_y = -0.15f;
+    float offset_y = 0.0f;
 
     // Arbitrarily populate this with size later on
     terrain.vertices = {
