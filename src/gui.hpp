75e22115601777a9c47d7a4fdadba7089c06fbf7
Charles Wang <zwcharl@gmail.com>
2025-12-01 16:31:04 -0500

    feat: thing

--- src/gui.hpp
+++ src/gui.hpp
@@ -54,6 +54,11 @@ struct Gui {
         bool enable_debug = false;
         bool enable_ao = false;
     } ao = {};
+
+    struct TonemappingData {
+        bool use_sdr_mode = false;
+        float hdr_target_luminance = 1000.f;
+    } tonemapping = {};
 };
 
 Gui initialize( Context& ctx, const engine::State& engine );
