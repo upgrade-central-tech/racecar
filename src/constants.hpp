3680507e1f9ab961aa92e3108f04bc602aa8d023
Anthony Ge <geant@seas.upenn.edu>
2025-12-04 02:47:51 -0500

    moved irradiance logic to baker... with many assumptions

--- src/constants.hpp
+++ src/constants.hpp
@@ -2,7 +2,7 @@
 
 namespace racecar::constant {
 
-constexpr int SCREEN_W = 1280;
-constexpr int SCREEN_H = 720;
+constexpr int SCREEN_W = 1920;
+constexpr int SCREEN_H = 1080;
 
 } // namespace racecar::constant
