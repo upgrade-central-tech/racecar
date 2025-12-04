56a3d1c796c80924ff79c4e8c846d155f9b31ab9
Charles Wang <zwcharl@gmail.com>
2025-12-03 22:30:13 -0500

    feat: separate out common shader code, reorganize into sky/geo folders, begin `get_sun_and_sky_irradiance`

--- shaders/atmosphere/sky/compile_atms_sky.ps1
+++ shaders/atmosphere/sky/compile_atms_sky.ps1
@@ -1,5 +1,9 @@
 # Assumes compiler is on your PATH (which it should be)
-slangc.exe "$PSScriptRoot\main.slang" -target spirv -profile spirv_1_4 -o "$PSScriptRoot\atmosphere.spv"
-slangc.exe "$PSScriptRoot\bake_atmosphere.slang" -target spirv -profile spirv_1_5 -emit-spirv-directly -fvk-use-entrypoint-name -entry cs_bake_atmosphere -capability SPV_EXT_shader_atomic_float_add -o "$PSScriptRoot\bake_atmosphere.spv"
-slangc.exe "$PSScriptRoot\bake_atmosphere.slang" -target spirv -profile spirv_1_5 -emit-spirv-directly -fvk-use-entrypoint-name -entry cs_bake_atmosphere_irradiance -capability SPV_EXT_shader_atomic_float_add -o "$PSScriptRoot\bake_atmosphere_irradiance.spv"
-slangc.exe "$PSScriptRoot\bake_atmosphere.slang" -target spirv -profile spirv_1_5 -emit-spirv-directly -fvk-use-entrypoint-name -entry cs_bake_atmosphere_mips -capability SPV_EXT_shader_atomic_float_add -o "$PSScriptRoot\bake_atmosphere_mips.spv"
+
+$IncludePath = Resolve-Path -Path "$PSScriptRoot\.."
+$CommonArgs = "-I", $IncludePath, "-target", "spirv", "-profile", "spirv_1_5"
+
+slangc.exe "$PSScriptRoot\main.slang" @CommonArgs -o "$PSScriptRoot\atmosphere.spv"
+slangc.exe "$PSScriptRoot\bake_atmosphere.slang" @CommonArgs -fvk-use-entrypoint-name -entry cs_bake_atmosphere -capability SPV_EXT_shader_atomic_float_add -o "$PSScriptRoot\bake_atmosphere.spv"
+slangc.exe "$PSScriptRoot\bake_atmosphere.slang" @CommonArgs -fvk-use-entrypoint-name -entry cs_bake_atmosphere_irradiance -capability SPV_EXT_shader_atomic_float_add -o "$PSScriptRoot\bake_atmosphere_irradiance.spv"
+slangc.exe "$PSScriptRoot\bake_atmosphere.slang" @CommonArgs -fvk-use-entrypoint-name -entry cs_bake_atmosphere_mips -capability SPV_EXT_shader_atomic_float_add -o "$PSScriptRoot\bake_atmosphere_mips.spv"
