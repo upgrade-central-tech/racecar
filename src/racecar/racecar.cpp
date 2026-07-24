abd09c4892374d21bb915f0f5f595b4c86c58e8b
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2026-07-24 15:31:57 -0700

    extracted padded vertex dataa craetoin

--- src/racecar/racecar.cpp
+++ src/racecar/racecar.cpp
@@ -1032,6 +1032,57 @@ void build_car_tlas( engine::State& engine, VkCommandBuffer& precompute_cmdbuf )
     vk::rt::build_tlas( precompute_cmdbuf, engine.tlas );
 }
 
+vk::mem::AllocatedBuffer create_padded_vertex_data_buffer(
+    Context& ctx, engine::State& engine, geometry::scene::Mesh& scene_mesh
+)
+{
+    std::vector<ub_data::PaddedVertex> padded_vertex_data { };
+    for ( geometry::scene::Vertex& v : scene_mesh.vertices ) {
+        padded_vertex_data.push_back(
+            { .position = v.position, .normal = v.normal, .tangent = v.tangent, .uv = v.uv }
+        );
+    }
+
+    vk::mem::AllocatedBuffer padded_vertex_data_buffer = vk::mem::create_buffer(
+        ctx.vulkan,
+        padded_vertex_data.size() * sizeof( ub_data::PaddedVertex ),
+        VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        VMA_MEMORY_USAGE_CPU_TO_GPU
+    );
+
+    vk::mem::AllocatedBuffer staging = vk::mem::create_buffer(
+        ctx.vulkan,
+        padded_vertex_data.size() * sizeof( ub_data::PaddedVertex ),
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VMA_MEMORY_USAGE_CPU_ONLY
+    );
+
+    void* data = staging.info.pMappedData;
+    std::memcpy(
+        data,
+        padded_vertex_data.data(),
+        padded_vertex_data.size() * sizeof( ub_data::PaddedVertex )
+    );
+
+    engine::immediate_submit( ctx.vulkan, engine.immediate_submit, [&]( VkCommandBuffer cmd_buf ) {
+        VkBufferCopy vertex_buf_copy = {
+            .srcOffset = 0,
+            .dstOffset = 0,
+            .size = padded_vertex_data.size() * sizeof( ub_data::PaddedVertex ),
+        };
+
+        vkCmdCopyBuffer(
+            cmd_buf,
+            staging.handle,
+            padded_vertex_data_buffer.handle,
+            1,
+            &vertex_buf_copy
+        );
+    } );
+
+    return padded_vertex_data_buffer;
+}
+
 void run( bool use_fullscreen )
 {
     // ================================================================================================================
@@ -1301,55 +1352,7 @@ void run( bool use_fullscreen )
         VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT
     );
 
-    std::vector<ub_data::PaddedVertex> padded_vertex_data { };
-    for ( geometry::scene::Vertex& v : scene_mesh.vertices ) {
-        padded_vertex_data.push_back(
-            { .position = v.position, .normal = v.normal, .tangent = v.tangent, .uv = v.uv }
-        );
-    }
-
-    vk::mem::AllocatedBuffer padded_vertex_data_buffer = vk::mem::create_buffer(
-        ctx.vulkan,
-        padded_vertex_data.size() * sizeof( ub_data::PaddedVertex ),
-        VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
-        VMA_MEMORY_USAGE_CPU_TO_GPU
-    );
-
-    {
-        vk::mem::AllocatedBuffer staging = vk::mem::create_buffer(
-            ctx.vulkan,
-            padded_vertex_data.size() * sizeof( ub_data::PaddedVertex ),
-            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-            VMA_MEMORY_USAGE_CPU_ONLY
-        );
-
-        void* data = staging.info.pMappedData;
-        std::memcpy(
-            data,
-            padded_vertex_data.data(),
-            padded_vertex_data.size() * sizeof( ub_data::PaddedVertex )
-        );
-
-        engine::immediate_submit(
-            ctx.vulkan,
-            engine.immediate_submit,
-            [&]( VkCommandBuffer cmd_buf ) {
-                VkBufferCopy vertex_buf_copy = {
-                    .srcOffset = 0,
-                    .dstOffset = 0,
-                    .size = padded_vertex_data.size() * sizeof( ub_data::PaddedVertex ),
-                };
-
-                vkCmdCopyBuffer(
-                    cmd_buf,
-                    staging.handle,
-                    padded_vertex_data_buffer.handle,
-                    1,
-                    &vertex_buf_copy
-                );
-            }
-        );
-    }
+    vk::mem::AllocatedBuffer padded_vertex_data_buffer = create_padded_vertex_data_buffer(ctx, engine, scene_mesh);
 
     engine::update_descriptor_set_const_storage_buffer(
         ctx.vulkan,
@@ -1401,6 +1404,17 @@ void run( bool use_fullscreen )
         );
     }
 
+    UniformBuffer<ub_data::RTTextureUniform> rt_texture_uniform_data
+        = create_uniform_buffer( ctx.vulkan, rt_texture_uniform, engine.frame_overlap );
+    rt_texture_uniform_data.set_data( rt_texture_uniform );
+    engine::update_descriptor_set_uniform(
+        ctx.vulkan,
+        engine,
+        car_descriptor_set,
+        rt_texture_uniform_data,
+        3
+    );
+
     // number of albedo textures to bind
     engine::DescriptorSet combined_textures_desc_set = engine::generate_array_descriptor_set(
         ctx.vulkan,
@@ -1426,17 +1440,6 @@ void run( bool use_fullscreen )
         combined_textures_desc_set,
         metallic_roughness_textures,
         1
-    );
-
-    UniformBuffer<ub_data::RTTextureUniform> rt_texture_uniform_data
-        = create_uniform_buffer( ctx.vulkan, rt_texture_uniform, engine.frame_overlap );
-    rt_texture_uniform_data.set_data( rt_texture_uniform );
-    engine::update_descriptor_set_uniform(
-        ctx.vulkan,
-        engine,
-        car_descriptor_set,
-        rt_texture_uniform_data,
-        3
     );
 
     // reflection data pass
