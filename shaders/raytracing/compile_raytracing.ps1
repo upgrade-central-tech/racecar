1eec294e2109dcf4dc4dcf42e4b519e18a74fae5
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2026-08-04 00:04:16 -0700

    added rayquery support

--- shaders/raytracing/compile_raytracing.ps1
+++ shaders/raytracing/compile_raytracing.ps1
@@ -1,2 +1,2 @@
 # Assumes compiler is on your PATH (which it should be)
-../../../slang/bin/slangc.exe "$PSScriptRoot\shadow.slang" -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vs_main -entry fs_main -o "$PSScriptRoot\shadow.spv"
+../../../slang/bin/slangc.exe "$PSScriptRoot\shadow.slang" -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -capability spvRayQueryKHR -entry vs_main -entry fs_main -o "$PSScriptRoot\shadow.spv"
--- shaders/reflections/compile_reflections.ps1
+++ shaders/reflections/compile_reflections.ps1
@@ -1,2 +1,2 @@
 # Assumes compiler is on your PATH (which it should be)
-../../../slang/bin/slangc.exe "$PSScriptRoot\reflections.slang" -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vs_main -entry fs_main -o "$PSScriptRoot\reflections.spv"
+../../../slang/bin/slangc.exe "$PSScriptRoot\reflections.slang" -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -capability spvRayQueryKHR -entry vs_main -entry fs_main -o "$PSScriptRoot\reflections.spv"
